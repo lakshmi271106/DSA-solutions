1232615a3bf2b5922034cf3659ef33bfa06b05d7
lakshmi271106 <yblakshmiyandamuri@gmail.com>
2026-05-30 11:36:25 +0530

    Create GenerateParanthesis.cpp

---
+++ Recursion_Backtracking/GenerateParanthesis.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void backtrack(string curr, int open, int close, int n) {
+
+        // Base case
+        if(curr.length() == 2 * n) {
+            ans.push_back(curr);
+            return;
+        }
+
+        // Choose '('
+        if(open < n) {
+            backtrack(curr + "(", open + 1, close, n);
+        }
+
+        // Choose ')'
+        if(close < open) {
+            backtrack(curr + ")", open, close + 1, n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+
+        backtrack("", 0, 0, n);
+
+        return ans;
+    }
+};
